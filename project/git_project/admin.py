ea933fe7129d29c508b7d704a827ec7156177b13
pajevicn <pajevicn@gmail.com>
2017-02-10 14:25:57 +0100

    izmenijenjen model, promijenjen front, osnovne funkcionalnosti

--- project/git_project/admin.py
+++ project/git_project/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Project, Issue, Label, Commit, Profile
 
-# Register your models here.
+admin.site.register(Project)
+admin.site.register(Issue)
+admin.site.register(Label)
+admin.site.register(Commit)
+admin.site.register(Profile)
